d69dd20cf12394df4c7440fd2a83c620f6f095e3
anonviranon <anonviranon@gmail.com>
2024-12-12 22:06:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const gsheetdb = require('gsheetdb');
+const db = new gsheetdb({
+  spreadsheetId: '1Jc9TsdJwvmCdaRSIeasMOtDD_OLBs2keyMgUcsm7F8g',
+  sheetName: 'test',
+  credentialsJSON: require('"C:\Users\abubumusyik\Desktop\masterlist-444513-6e21f708b0a6.json"')
+});
+
+async function getData() {
+  return await db.getData();
+}
+
+async function fetchData() {
+  const data = await getData();
+  const tableBody = document.getElementById('tableBody');
+  data.forEach(row => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${row.date}</td><td>${row.amount}</td><td>${row.description}</td>`;
+    tableBody.appendChild(tr);
+  });
+}
+fetchData();
 
